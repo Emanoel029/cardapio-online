1e31a948ec934f7ae3384c4d6965d0af636164e6
Emanoel Timbó <emanoelpontes8@gmail.com>
2023-12-29 00:30:13 -0300

    Codificado manipulação de etapas do carrinho em JS

--- js/app.js
+++ js/app.js
@@ -105,10 +105,112 @@ cardapio.metodos = {
           MEU_CARRINHO.push(item[0]);
         }
 
-        alert("Item adicionado ao carrinho");
+        cardapio.metodos.mensagem("Item adicionado ao carrinho", "green");
         $("#qntd-" + id).text(0);
+
+        cardapio.metodos.atualizarBadgeTotal();
       }
     }
+  },
+
+  //Atualiza o badge de totais dos botões "Meu carrinho"
+  atualizarBadgeTotal: () => {
+    var total = 0;
+
+    $.each(MEU_CARRINHO, (i, e) => {
+      total += e.qntd;
+    });
+
+    if (total > 0) {
+      $(".botao-carrinho").removeClass("hidden");
+      $(".container-total-carrinho").removeClass("hidden");
+    } else {
+      $(".botao-carrinho").addClass("hidden");
+      $(".container-total-carrinho").addClass("hidden");
+    }
+    $(".badge-total-carrinho").html(total);
+  },
+
+  //Abrir a modal de carrinho
+  abrirCarrinho: (abrir) => {
+    if (abrir) {
+      $("#modalCarrinho").removeClass("hidden");
+      cardapio.metodos.carregarEtapa(1);
+    } else {
+      $("#modalCarrinho").addClass("hidden");
+    }
+  },
+
+  //Altera os textos e exibe os botões das etapas
+  carregarEtapa: (etapa) => {
+    if (etapa == 1) {
+      $("#ldlTituloEtapa").text("Seu carrinho:");
+      $("#itensCarrinho").removeClass("hidden");
+      $("#localEntrega").addClass("hidden");
+      $("#resumeCarrinho").addClass("hidden");
+
+      $(".etapa").removeClass("active");
+      $(".etapa1").addClass("active");
+
+      $("#btnEtapaPedido").removeClass("hidden");
+      $("#btnEtapaEndereco").addClass("hidden");
+      $("#btnEtapaResumo").addClass("hidden");
+      $("#btnVoltar").addClass("hidden");
+    }
+    if (etapa == 2) {
+      $("#ldlTituloEtapa").text("Endereço de entrega:");
+      $("#itensCarrinho").addClass("hidden");
+      $("#localEntrega").removeClass("hidden");
+      $("#resumeCarrinho").addClass("hidden");
+
+      $(".etapa").removeClass("active");
+      $(".etapa1").addClass("active");
+      $(".etapa2").addClass("active");
+
+      $("#btnEtapaPedido").addClass("hidden");
+      $("#btnEtapaEndereco").removeClass("hidden");
+      $("#btnEtapaResumo").addClass("hidden");
+      $("#btnVoltar").removeClass("hidden");
+    }
+    if (etapa == 3) {
+      $("#ldlTituloEtapa").text("Resumo do pedido:");
+      $("#itensCarrinho").addClass("hidden");
+      $("#localEntrega").addClass("hidden");
+      $("#resumeCarrinho").removeClass("hidden");
+
+      $(".etapa").removeClass("active");
+      $(".etapa1").addClass("active");
+      $(".etapa2").addClass("active");
+      $(".etapa3").addClass("active");
+
+      $("#btnEtapaPedido").addClass("hidden");
+      $("#btnEtapaEndereco").addClass("hidden");
+      $("#btnEtapaResumo").removeClass("hidden");
+      $("#btnVoltar").removeClass("hidden");
+    }
+  },
+
+  //Botão de volta etapa
+  voltarEtapa: () => {
+    let etapa = $(".etapa.active").length;
+    cardapio.metodos.carregarEtapa(etapa - 1);
+  },
+
+  //mensagens
+  mensagem: (texto, cor = "red", tempo = 3500) => {
+    let id = Math.floor(Date.now() * Math.random()).toString(); //criando um id
+
+    let msg = `<div id="msg-${id}" class="animated fadeInDown toast ${cor}">${texto}</div>`;
+
+    $("#container-mensagens").append(msg);
+
+    setTimeout(() => {
+      $("#msg-" + id).removeClass("fadeInDown");
+      $("#msg-" + id).addClass("fadeOutUp");
+      setTimeout(() => {
+        $("#msg-" + id).remove();
+      }, 800);
+    }, tempo);
   },
 };
 
