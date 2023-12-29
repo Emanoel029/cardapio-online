13156e57f5957dfee51af4540fec60e339827bfe
Emanoel Timbó <emanoelpontes8@gmail.com>
2023-12-29 10:14:48 -0300

    Manipulando Itens do Carrinho com JS

--- js/app.js
+++ js/app.js
@@ -135,7 +135,7 @@ cardapio.metodos = {
   abrirCarrinho: (abrir) => {
     if (abrir) {
       $("#modalCarrinho").removeClass("hidden");
-      cardapio.metodos.carregarEtapa(1);
+      cardapio.metodos.carregarCarrinho();
     } else {
       $("#modalCarrinho").addClass("hidden");
     }
@@ -196,6 +196,68 @@ cardapio.metodos = {
     cardapio.metodos.carregarEtapa(etapa - 1);
   },
 
+  //carregar a lista de itens do carrinho
+  carregarCarrinho: () => {
+    cardapio.metodos.carregarEtapa(1);
+
+    if (MEU_CARRINHO.length > 0) {
+      $("#itensCarrinho").html("");
+
+      $.each(MEU_CARRINHO, (i, e) => {
+        let temp = cardapio.templetes.itemCarrinho
+          .replace(/\${img}/g, e.img)
+          .replace(/\${name}/g, e.name)
+          .replace(/\${price}/g, e.price.toFixed(2).replace(".", ","))
+          .replace(/\${id}/g, e.id)
+          .replace(/\${qntd}/g, e.qntd);
+
+        $("#itensCarrinho").append(temp);
+      });
+    } else {
+      $("#itensCarrinho").html(
+        "<p class='carrinho-vazio'><i class='fa fa-shopping-bag'></i> Seu carrinho está vazio.</p>"
+      );
+    }
+  },
+  //Diminuir quantidade do item no carrinho
+  diminuirQuantidadeCarrinho: (id) => {
+    let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+
+    if (qntdAtual > 1) {
+      $("#qntd-carrinho-" + id).text(qntdAtual - 1);
+      cardapio.metodos.atualizarCarrinho(id, qntdAtual - 1);
+    } else {
+      cardapio.metodos.removerItemCarrinho(id);
+    }
+  },
+
+  //Aumentar quantidade do item no carrinho
+  aumentarQuantidadeCarrinho: (id) => {
+    let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+    $("#qntd-carrinho-" + id).text(qntdAtual + 1);
+    cardapio.metodos.atualizarCarrinho(id, qntdAtual + 1);
+  },
+
+  //Botão de remover item do carrinho
+  removerItemCarrinho: (id) => {
+    MEU_CARRINHO = $.grep(MEU_CARRINHO, (e, i) => {
+      return e.id != id;
+    });
+    cardapio.metodos.carregarCarrinho();
+
+    //Atualiza o botão carrinho com a quantidade atualizada
+    cardapio.metodos.atualizarBadgeTotal();
+  },
+
+  //Atualiza o carrinho com a quantidade atual
+  atualizarCarrinho: (id, qntd) => {
+    let objIndex = MEU_CARRINHO.findIndex((obj) => obj.id == id);
+    MEU_CARRINHO[objIndex].qntd = qntd;
+
+    //Atualiza o botão carrinho com a quantidade atualizada
+    cardapio.metodos.atualizarBadgeTotal();
+  },
+
   //mensagens
   mensagem: (texto, cor = "red", tempo = 3500) => {
     let id = Math.floor(Date.now() * Math.random()).toString(); //criando um id
@@ -235,5 +297,25 @@ cardapio.templetes = {
             </div>
           </div>
         </div>
+  `,
+
+  itemCarrinho: `
+          <div class="col-12 item-carrinho">
+            <div class="img-produto">
+              <img src="\${img}"/>
+            </div>
+            <div class="dados-produto">
+              <p class="title-produto"><b>\${name}</b></p>
+              <p class="price-produto"><b>R$ \${price}</b></p>
+            </div>
+            <div class="add-carrinho">
+            <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidadeCarrinho('\${id}')"><i class="fas fa-minus"></i></span>
+            <span class="add-numero-itens" id="qntd-carrinho-\${id}">\${qntd}</span>
+            <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')"><i class="fas fa-plus"></i></span>
+            <span class="btn btn-remove" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="fa fa-times"></i></span>
+            </div>
+          </div>
+
+
   `,
 };
