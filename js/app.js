74c5322bdf659bb7979763988155b816a9b2f3c0
Emanoel Timbó <emanoelpontes8@gmail.com>
2023-12-30 21:09:41 -0300

    Animando o site com Animate.css e Wow.js

--- js/app.js
+++ js/app.js
@@ -527,7 +527,7 @@ cardapio.metodos = {
 
 cardapio.templetes = {
   item: `
-        <div class="col-3 mb-5">
+        <div class="col-3 mb-5 animated fadeInUp">
           <div class="card card-item" id="\${id}">
             <div class="img-produto">
               <img src="\${img}"/>
